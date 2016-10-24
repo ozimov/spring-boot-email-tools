92c095b4dc5113dfd1ab8aceee12b2f4a4c48354
Roberto Trunfio <roberto.trunfio@gmail.com>
2016-10-24 23:37:05 +0200

    Added scheduling exercise using Spring scheduler

--- examples/autowiring-email-service-example/src/main/java/com/test/Application.java
+++ examples/autowiring-email-service-example/src/main/java/com/test/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.test", "it.ozimov.springboot"})
+@EnableScheduling
 @EnableAsync
 public class Application {
 
