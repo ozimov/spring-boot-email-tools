9a8a770d9449bf00639a46b38f084a85ec4fc4fe
Roberto Trunfio <roberto.trunfio@gmail.com>
2017-04-02 17:54:44 +0200

    Added logging example

---
+++ examples/email-logging-example/src/main/java/com/test/PlainTextEmailWithPrettyLogsApplication.java
@@ -0,0 +1,54 @@
+package com.test;
+
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import javax.annotation.PostConstruct;
+import java.io.UnsupportedEncodingException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+@EnableEmailTools
+public class PlainTextEmailWithPrettyLogsApplication implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private TestService testService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlainTextEmailWithPrettyLogsApplication.class, args);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @PostConstruct
+    public void sendEmail() throws UnsupportedEncodingException, InterruptedException {
+        testService.sendPlainTextEmailSoThatYouCanSeePrettyLogs();
+
+        close();
+    }
+
+    private void close() {
+        TimerTask shutdownTask = new TimerTask() {
+            @Override
+            public void run() {
+                ((AbstractApplicationContext) applicationContext).close();
+            }
+        };
+        Timer shutdownTimer = new Timer();
+        shutdownTimer.schedule(shutdownTask, TimeUnit.SECONDS.toMillis(3));
+    }
+
+}
---
+++ examples/email-logging-example/src/main/java/com/test/TestService.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import it.ozimov.springboot.mail.model.Email;
+import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+import it.ozimov.springboot.mail.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.InternetAddress;
+import java.io.UnsupportedEncodingException;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+@Service
+public class TestService {
+
+    @Autowired
+    private EmailService emailService;
+
+    public void sendPlainTextEmailSoThatYouCanSeePrettyLogs() throws UnsupportedEncodingException {
+        final Email email = DefaultEmail.builder()
+                .from(new InternetAddress("hari.seldon@gmail.com",
+                        "Hari Seldon"))
+                .to(newArrayList(
+                        new InternetAddress("roberto.trunfio@gmail.com",
+                                "Cleon I"),
+                        new InternetAddress("the-real-cleon@trantor.gov",
+                                "Cleon I")))
+                .subject("You shall die! It's not me, it's Psychohistory")
+                .body("Hello Planet!")
+                .encoding("UTF-8").build();
+
+        emailService.send(email);
+    }
+
+}
